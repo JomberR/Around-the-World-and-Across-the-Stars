37fb6bbf5e4f2619f665b751341f3cd39eb7345e
JomberR <60245648+JomberR@users.noreply.github.com>
2024-07-07 20:58:46 -0500

    Menu Stuff

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useState } from 'react';
 
+ 
 export default function MainMenu(){
+    const [visibleMenu, setVisibleMenu] = useState({
+        menu: "startMenu"
+    });
+
     return(
         <div>
             <div className="Main-menu-title">
@@ -8,14 +14,82 @@ export default function MainMenu(){
             </div>
             <div className="Grid-container Grid-items-center Center-page">
                 <div className="Vertical-buttons">
-                <button onClick={() => alert("Not implemented!")}>
-                    Continue
-                </button>
-                <button>
-                    New Game
-                </button>
+
+                    <StartMenu/>
+                    <LevelSelectMenu/>
                 </div>
             </div>
         </div>
     );
+
+    function GoToLevelSelect(){
+        setVisibleMenu({menu: "levelSelect"});
+    }
+
+    function GoToStartMenu(){
+        setVisibleMenu({menu: "startMenu"});
+    }
+
+    //Local Components
+    function StartMenu(){
+        if (visibleMenu.menu === "startMenu"){
+            return(
+                <div>
+                    <div>
+                        <button onClick={() => alert("Not implemented!")}>
+                            Continue
+                        </button>
+                    </div>
+                    <div>
+                        <button onClick={GoToLevelSelect}>
+                            New Game
+                        </button>
+                    </div>
+                    
+                </div>
+
+            );
+        }
+        else{
+            return(null);
+        }
+    }
+
+    function LevelSelectMenu(){
+        if (visibleMenu.menu === "levelSelect"){
+            return(
+                <div>
+                    <div>
+                        <button onClick={GoToStartMenu}>
+                            Back
+                        </button>
+                    </div>
+                    <div>
+                        <button>
+                            Frozen Foglands
+                        </button>
+                    </div>
+                    <div>
+                        <button disabled>
+                            The Deep Dark Down Below
+                        </button>
+                    </div>
+                    <div>
+                        <button disabled>
+                            Crystal Caverns
+                        </button>
+                    </div>
+                    <div>
+                        <button disabled>
+                            Last Light
+                        </button>
+                    </div>
+                </div>
+            );
+        }
+        else{
+            return(null);
+        }
+    }
 }
+
