4ddafab4352a86382669c87deec2d8dfac5b32db
JomberR <60245648+JomberR@users.noreply.github.com>
2024-07-12 14:10:03 -0500

    Hex layout for locations

--- src/display/components/scenes/MapFrozenFoglands.js
+++ src/display/components/scenes/MapFrozenFoglands.js
@@ -9,17 +9,32 @@ export default function MapFrozenFoglands(){
         <div>
             <Header locationName="The Frozen Foglands"/>
 
-            <div className="Grid-container Grid-container-3-col">
-                <div className="Grid-item-center">
+            <div className="Grid-container Hex-layout">
+
+                <div className="Grid-item-center Hex-layout-item1">
                     <FieldHospital/>
                 </div>
-                <div className="Grid-item-center">
-                    <GreatWindmill className="Grid-item-center"/>
+                <div className="Grid-item-center Hex-layout-item2">
+                    <FieldHospital/>
                 </div>
 
-                <div className="Grid-item-center">
+                <div className="Grid-item-center Hex-layout-item3">
                     <FieldHospital/>
                 </div>
+                <div className="Grid-item-center Hex-layout-item4">
+                    <GreatWindmill className="Grid-item-center"/>
+                </div>
+                <div className="Grid-item-center Hex-layout-item5">
+                    <FieldHospital/>
+                </div>
+
+                <div className="Grid-item-center Hex-layout-item6">
+                    <FieldHospital/>
+                </div>
+                <div className="Grid-item-center Hex-layout-item7">
+                    <FieldHospital/>
+                </div>
+                
             </div>
 
             <Footer/>
